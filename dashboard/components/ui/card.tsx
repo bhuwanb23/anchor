ea91cb9a51bcd0a0fd98db6439b5f0c29466823b
bhuwanb23 <bhuwanseervi4567@gmail.com>
2026-07-29 20:22:33 +0530

    refactor(card): simplify component props by removing unnecessary interfaces

--- dashboard/components/ui/card.tsx
+++ dashboard/components/ui/card.tsx
@@ -1,25 +1,19 @@
 import { type HTMLAttributes, forwardRef } from "react";
 
-interface CardProps extends HTMLAttributes<HTMLDivElement> {}
-
-const Card = forwardRef<HTMLDivElement, CardProps>(
-  ({ className = "", children, ...props }, ref) => {
-    return (
-      <div
-        ref={ref}
-        className={`rounded-xl border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-900 ${className}`}
-        {...props}
-      >
-        {children}
-      </div>
-    );
-  }
+const Card = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+  ({ className = "", children, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={`rounded-xl border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-900 ${className}`}
+      {...props}
+    >
+      {children}
+    </div>
+  )
 );
-
 Card.displayName = "Card";
 
-interface CardHeaderProps extends HTMLAttributes<HTMLDivElement> {}
-const CardHeader = forwardRef<HTMLDivElement, CardHeaderProps>(
+const CardHeader = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
   ({ className = "", children, ...props }, ref) => (
     <div ref={ref} className={`mb-4 ${className}`} {...props}>
       {children}
@@ -28,8 +22,7 @@ const CardHeader = forwardRef<HTMLDivElement, CardHeaderProps>(
 );
 CardHeader.displayName = "CardHeader";
 
-interface CardTitleProps extends HTMLAttributes<HTMLHeadingElement> {}
-const CardTitle = forwardRef<HTMLHeadingElement, CardTitleProps>(
+const CardTitle = forwardRef<HTMLHeadingElement, HTMLAttributes<HTMLHeadingElement>>(
   ({ className = "", children, ...props }, ref) => (
     <h3
       ref={ref}
@@ -42,8 +35,7 @@ const CardTitle = forwardRef<HTMLHeadingElement, CardTitleProps>(
 );
 CardTitle.displayName = "CardTitle";
 
-interface CardContentProps extends HTMLAttributes<HTMLDivElement> {}
-const CardContent = forwardRef<HTMLDivElement, CardContentProps>(
+const CardContent = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
   ({ className = "", children, ...props }, ref) => (
     <div ref={ref} className={className} {...props}>
       {children}
