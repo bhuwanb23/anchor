e9fc3e866bf28dfde8d79fd2939eb9738b243503
bhuwanb23 <bhuwanseervi4567@gmail.com>
2026-08-06 22:20:27 +0530

    feat(onboarding): create OnboardingPage component to redirect to connect server

---
+++ dashboard/app/onboarding/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function OnboardingPage() {
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/onboarding/connect-server");
+  }, [router]);
+  return null;
+}
