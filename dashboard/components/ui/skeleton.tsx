f1b971c55f1a0ab282b4ab695e024246b4999dd6
bhuwanb23 <bhuwanseervi4567@gmail.com>
2026-08-06 22:22:43 +0530

    feat(skeleton): add Skeleton component for loading placeholders

---
+++ dashboard/components/ui/skeleton.tsx
@@ -0,0 +1,5 @@
+import { cn } from "@/lib/utils";
+
+export function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={`animate-pulse rounded bg-gray-200 dark:bg-gray-800 ${className || ""}`} {...props} />;
+}
